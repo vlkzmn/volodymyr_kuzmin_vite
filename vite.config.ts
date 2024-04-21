509acbf6e7d5faa64b99a9a69f11eade9ccc5469
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-04-21 20:00:46 +0300

    Changed base url to https://vlkzmn.github.io/volodymyr_kuzmin_vite/

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://volodymyr-kuzmin.pp.ua/',
+  base: 'https://vlkzmn.github.io/volodymyr_kuzmin_vite/',
   server: {
     open: true,
   },
